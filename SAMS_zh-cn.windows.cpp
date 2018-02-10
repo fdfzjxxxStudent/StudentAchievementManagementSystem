4299f0acbbb09481e7924a431d6963e72e906245
XiyuWang <36125855+XiyuWang2006@users.noreply.github.com>
2018-02-10 10:19:57 +0800

    Update SAMS_zh-cn.windows.cpp

--- SAMS_zh-cn.windows.cpp
+++ SAMS_zh-cn.windows.cpp
@@ -1,5 +1,4 @@
 //Copyright (C) XiyuWang 2017-2018 All rights reserved.
-//Copyright (C) 学生成绩管理系统 2017-2018 All rights reserved.
 //Version: 16.9.18
 
 //Author: XiyuWang
@@ -959,7 +958,6 @@ int main(){
 			o[0]=0; 
 			system("cls");	
 			cout<<"Copyright (C) XiyuWang 2017-"<<t->tm_year+1900<<" All rights reserved."<<endl;
-			cout<<"Copyright (C) 学生成绩管理系统 2017-"<<t->tm_year+1900<<" All rights reserved."<<endl;
 			cout<<"Version:16.9.18"<<endl; 
 			system("pause");
 			o[0]=0; 
