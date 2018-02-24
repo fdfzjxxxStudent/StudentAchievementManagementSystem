4adad51f121a2d9d2f5d4db81bd14df95fcc7705
XiyuWang <36125855+XiyuWang2006@users.noreply.github.com>
2018-02-24 19:24:23 +0800

    Update SAMS_zh-cn Windows Edition

--- WindowsEdition/SAMS_zh-cn.windows.cpp
+++ WindowsEdition/SAMS_zh-cn.windows.cpp
@@ -1,4 +1,4 @@
-//Copyright (C) 2018 XiyuWang All rights reserved.
+//Copyright (C) 2017-2018 XiyuWang All rights reserved.
 //Version: 16.9.18
 
 //Author: XiyuWang
@@ -115,43 +115,15 @@ void load(){
 namespace user{ 
 	//user operation 
 	void reg(){
-		ofstream fout;
-		fout.open("C:\\ProgramData\\StudentAchievementManagementSystem\\Welcome.txt");
-		fout<<"欢迎使用学生成绩管理系统，这是一个警告文件，请勿删除 C:\\ProgramData\\StudentAchievementManagementSystem\\ ，一旦删除，系统将崩溃！"<<endl; 
-		fout.close();
-		system("mkdir C:\\ProgramData\\StudentAchievementManagementSystem");
-		system("cmd.exe /c attrib C:\\ProgramData\\StudentAchievementManagementSystem\\ +h");
-		system("title 学生成绩管理系统");
-		system("cls");
-		cout<<"您尚未注册，请输入用户名：";
-		cin>>o;
-		fout.open("C:\\ProgramData\\StudentAchievementManagementSystem\\~Control.usr");
-		if(!fout){
-			MessageBox(NULL,"系统错误！\r\n请以管理员身份重新运行程序！","学生成绩管理系统",MB_ICONERROR|MB_SYSTEMMODAL|MB_SETFOREGROUND); 
+		ifstream fin;
+		fin.open("SignIn.dll"); 
+		if(!fin){
+			system("cls");
+			cout<<"系统重要文件缺失，无法注册账户！"<<endl;
+			system("pause");
 			exit(0);
 		}
-		fout<<o<<endl;
-		fout.close();
-		cout<<"请输入密码：";
-		cin>>o;
-		//加密算法：ASCII码加123
-		for(long long i=0;i<=o.size();i++)
-			o[i]=o[i]+123;
-		fout.open("C:\\ProgramData\\StudentAchievementManagementSystem\\~Control.passwd");
-		if(!fout){
-			MessageBox(NULL,"系统错误！\r\n请以管理员身份重新运行程序！","学生成绩管理系统",MB_ICONERROR|MB_SYSTEMMODAL|MB_SETFOREGROUND); 
-			exit(0);
-		}
-		fout<<o<<endl;
-		fout.close();
-		system("cmd.exe /c attrib C:\\ProgramData\\StudentAchievementManagementSystem\\~Control.info +h");
-		system("cmd.exe /c attrib C:\\ProgramData\\StudentAchievementManagementSystem\\~Control.passwd +h");
-		system("mkdir %USERPROFILE\\AppData\\Local\\SAMS");
-		fout.open("%USERPROFILE\\AppData\\Local\\SAMS\\~Reg");
-		fout<<1<<endl;
-		fout.close();
-		system("cls");
-		MessageBox(NULL,"注册成功！","学生成绩管理系统",MB_ICONINFORMATION|MB_SYSTEMMODAL|MB_SETFOREGROUND); 
+		system("SignIn.dll");
 	}
 	int wt;//wt:wrong_time
 	bool login(int t){
